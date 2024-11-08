da7d668f898067a69c27521b760ad638500b52c2
nantessa <nantyadipa@gmail.com>
2024-11-08 17:16:33 +0700

    change icons using react icons, and change button on modal order

--- components/OrderModalContent.tsx
+++ components/OrderModalContent.tsx
@@ -1,4 +1,7 @@
 import React, { ReactNode } from 'react';
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
+
 
 type ModalProps = {
   isOpen: boolean;
@@ -12,8 +15,8 @@ export function Modal({ isOpen, onClose, children }: ModalProps) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-lg p-2 relative w-full max-w-lg mx-auto">
-        <button onClick={onClose} className="absolute top-2 right-2 text-gray-500">
-          &times;
+        <button onClick={onClose} className="absolute top-2 right-2 text-gray-400">
+          <IoIosCloseCircleOutline size={"2rem"}/>
         </button>
         {children}
       </div>
